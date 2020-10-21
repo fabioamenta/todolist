4c389195bd0ca3fefd9f0cfe1ebe93efb5dd8e14
Fabio Amenta <famenta80@gmail.com>
2020-10-21 17:04:30 +0200

    Add Component files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "../styles/reset.css";
+import "../styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import components
+import List from "./List";
+import InputForm from "./InputForm";
+import Header from "./Header";
+
+class App extends Component {
+  // getInitialState
+  state = {
+    list: [],
+    pendingItem: ""
+  };
+
+  lastItemId = 0;
+
+  newItemId = () => {
+    const id = this.lastItemId;
+    this.lastItemId += 1;
+    return id;
+  };
+
+  // Flips isEditing bool
+  toggleIsEditingAt = id => {
+    console.log("isEditingAt", id);
+    this.setState({
+      list: this.state.list.map(item => {
+        if (id === item.id) {
+          return {
+            ...item,
+            isEditing: !item["isEditing"]
+          };
+        }
+        return item;
+      })
+    });
+  };
+
+  removeItemAt = id => {
+    this.setState({ list: this.state.list.filter(item => id !== item.id) });
+  };
+
+  handleItemInput = e => this.setState({ pendingItem: e.target.value });
+
+  // handle editing items
+  setNameAt = (name, id) => {
+    this.setState({
+      list: this.state.list.map(item => {
+        if (id === item.id) {
+          return {
+            ...item,
+            name
+          };
+        }
+        return item;
+      })
+    });
+  };
+
+  newItemSubmitHandler = e => {
+    e.preventDefault();
+    const id = this.newItemId();
+    this.setState({
+      list: [
+        {
+          name: this.state.pendingItem,
+          isEditing: false,
+          id
+        },
+        ...this.state.list
+      ],
+      pendingItem: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <InputForm
+          newItemSubmitHandler={this.newItemSubmitHandler}
+          handleItemInput={this.handleItemInput}
+          pendingItem={this.state.pendingItem}
+        />
+
+        <List
+          list={this.state.list}
+          removeItemAt={this.removeItemAt}
+          toggleIsEditingAt={this.toggleIsEditingAt}
+          setNameAt={this.setNameAt}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
