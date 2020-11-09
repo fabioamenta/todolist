ab836fc0e2074d77283b4ace72b9d4d702d0728d
Fabio Amenta <famenta80@gmail.com>
2020-11-09 17:20:24 +0100

    Update App structure and files link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import "../styles/reset.css";
-import "../styles/App.css";
+import "./styles/reset.css";
+import "./styles/App.css";
 
 // Import components
 import List from "./components/List/List";
@@ -8,92 +8,99 @@ import InputForm from "./components/InputForm/InputForm";
 import Header from "./components/Header/Header";
 
 class App extends Component {
-  // getInitialState
-  state = {
-    list: [],
-    pendingItem: ""
-  };
+	// getInitialState
+	state = {
+		list: [],
+		pendingItem: "",
+	};
 
-  lastItemId = 0;
+	lastItemId = 0;
 
-  newItemId = () => {
-    const id = this.lastItemId;
-    this.lastItemId += 1;
-    return id;
-  };
+	newItemId = () => {
+		const id = this.lastItemId;
+		this.lastItemId += 1;
+		return id;
+	};
 
-  // Flips isEditing bool
-  toggleIsEditingAt = id => {
-    console.log("isEditingAt", id);
-    this.setState({
-      list: this.state.list.map(item => {
-        if (id === item.id) {
-          return {
-            ...item,
-            isEditing: !item["isEditing"]
-          };
-        }
-        return item;
-      })
-    });
-  };
+	// Flips isEditing bool
+	toggleIsEditingAt = (id) => {
+		const { list } = this.state;
+		const updateList = list.map((item) => {
+			if (id === item.id) {
+				return {
+					...item,
+					isEditing: !item["isEditing"],
+				};
+			}
+			return item;
+		});
+		this.setState({
+			list: updateList,
+		});
+	};
 
-  removeItemAt = id => {
-    this.setState({ list: this.state.list.filter(item => id !== item.id) });
-  };
+	taskCompleted = (id) => {
+		console.log(id);
+		/* this.removeItemAt(id); */
+	};
 
-  handleItemInput = e => this.setState({ pendingItem: e.target.value });
+	removeItemAt = (id) => {
+		this.setState({ list: this.state.list.filter((item) => id !== item.id) });
+	};
 
-  // handle editing items
-  setNameAt = (name, id) => {
-    this.setState({
-      list: this.state.list.map(item => {
-        if (id === item.id) {
-          return {
-            ...item,
-            name
-          };
-        }
-        return item;
-      })
-    });
-  };
+	handleItemInput = (e) => this.setState({ pendingItem: e.target.value });
 
-  newItemSubmitHandler = e => {
-    e.preventDefault();
-    const id = this.newItemId();
-    this.setState({
-      list: [
-        {
-          name: this.state.pendingItem,
-          isEditing: false,
-          id
-        },
-        ...this.state.list
-      ],
-      pendingItem: ""
-    });
-  };
+	// handle editing items
+	setNameAt = (name, id) => {
+		this.setState({
+			list: this.state.list.map((item) => {
+				if (id === item.id) {
+					return {
+						...item,
+						name,
+					};
+				}
+				return item;
+			}),
+		});
+	};
 
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <InputForm
-          newItemSubmitHandler={this.newItemSubmitHandler}
-          handleItemInput={this.handleItemInput}
-          pendingItem={this.state.pendingItem}
-        />
+	newItemSubmitHandler = (e) => {
+		e.preventDefault();
+		const id = this.newItemId();
+		this.setState({
+			list: [
+				{
+					name: this.state.pendingItem,
+					isEditing: false,
+					id,
+				},
+				...this.state.list,
+			],
+			pendingItem: "",
+		});
+	};
 
-        <List
-          list={this.state.list}
-          removeItemAt={this.removeItemAt}
-          toggleIsEditingAt={this.toggleIsEditingAt}
-          setNameAt={this.setNameAt}
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='wrapper'>
+				<Header />
+				<InputForm
+					newItemSubmitHandler={this.newItemSubmitHandler}
+					handleItemInput={this.handleItemInput}
+					pendingItem={this.state.pendingItem}
+				/>
+
+				<List
+					list={this.state.list}
+					removeItemAt={this.removeItemAt}
+					toggleIsEditingAt={this.toggleIsEditingAt}
+					taskCompleted={this.taskCompleted}
+					setNameAt={this.setNameAt}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
